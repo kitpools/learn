d3282bd80ae50767788a8c74213883574395d30f
USER <USER@DESKTOP-G0T6VQN>
2020-10-05 02:49:05 +0900

    해결방법

--- src/kakaoblind2020/question1.java
+++ src/kakaoblind2020/question1.java
@@ -1,10 +1,19 @@
 package kakaoblind2020;
 
+
+// 문자열 s를 입력받으면 제일 앞에서부터 1개 단위로 잘라서 String변수 compress에 넣음
+// 반복문 시작 : 2개 단위로 잘라서 이 변수와 비교해서 더 짧은걸 compress에 넣음
+// ~ s.length/2개 단위로 자른거까지 반복
+// compress.length를 answer에 넣음
+// answer을 return
+
 public class question1 {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
-
+		
+		
+		
+		
 	}
 
 }
